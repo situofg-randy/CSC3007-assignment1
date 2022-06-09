041cca74c32ff92f7398ce2ebbb0007d373c3275
situofg-randy <71247128+situofg-randy@users.noreply.github.com>
2022-06-09 22:08:59 +0800

    Add files via upload

---
+++ useFetch.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./table.css"
+
+export default function UseFetch() {
+  const [data, setData] = useState({});
+
+  const url = "https://api.data.gov.sg/v1/environment/psi";
+
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState()
+  
+  const [headers, setHeaders] = useState([])
+  const [lastUpdate, setLastUpdate] = useState()
+
+  const [headersHtml, setHeadersHtml] = useState([])
+  const [rowsHtml, setRowsHtml] = useState([])
+  let headersHtmlArray = []
+  let rowsHtmlArray = []
+
+  useEffect(() => {
+    fetch(url)
+    .then((json) => json.json())
+    .then((res) => {
+      let date = new Date(res.items[0].update_timestamp)
+      let mins = ('0'+date.getMinutes()).slice(-2)
+      let months = date.toLocaleString('default', { month: 'short' });
+      setLastUpdate(date.getDay().toString()+" "+months+" "+date.getFullYear().toString()+", "+date.getHours()+":"+mins)
+      setData(res.items[0].readings)
+      setLoading(false)
+    }).catch(error => setError(error))
+  }, []);
+
+  useEffect(() => {
+    rowsHtmlArray = []
+    Object.keys(data).forEach((key) => {
+      rowsHtmlArray.push(
+        <tr>
+          <td>{key}</td>
+          <td>{data[key].national}</td>
+          <td>{data[key].central}</td>
+          <td>{data[key].west}</td>
+          <td>{data[key].east}</td>
+          <td>{data[key].north}</td>
+          <td>{data[key].south}</td>
+        </tr>
+      )
+    })
+
+    if (Object.keys(data).length > 0) {
+      setHeaders(Object.keys(Object.values(data)[0]))
+    }
+    console.log(data)
+    headersHtmlArray = []
+    headersHtmlArray.push(<th>metric</th>)
+    headersHtmlArray.push(<th>national</th>)
+    headersHtmlArray.push(<th>central</th>)
+    headersHtmlArray.push(<th>west</th>)
+    headersHtmlArray.push(<th>east</th>)
+    headersHtmlArray.push(<th>north</th>)
+    headersHtmlArray.push(<th>south</th>)
+    
+
+    setHeadersHtml(headersHtmlArray)
+    setRowsHtml(rowsHtmlArray)
+  }, [data])
+
+  if (loading) return <div>loading</div>
+
+  if (error) return <div>error</div>
+
+  return (
+    <div>
+      <h4>Last updated {lastUpdate} </h4>
+        <table>
+        <thead>
+          <tr>
+            {headersHtml}
+          </tr>
+        </thead>
+        <tbody>
+          {rowsHtml}
+        </tbody>
+      </table>
+    </div>
+  )  
+}
+
